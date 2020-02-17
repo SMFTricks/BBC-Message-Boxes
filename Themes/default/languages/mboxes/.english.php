79de3edf391e5de3093581580a260c94560103f2
Diego Andrés <diegoandres_cortes@outlook.com>
2020-02-17 17:26:22 -0600

    better this way
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/mboxes/.english.php
@@ -0,0 +1,25 @@
+<?php
+
+// BBC
+$txt['error_bbc'] = 'Add Error Box';
+$txt['warning_bbc'] = 'Add Warning Box';
+$txt['okay_bbc'] = 'Add Okay Box';
+$txt['info_bbc'] = 'Add Info Box';
+
+// Settings
+$txt['mboxes_settings'] = 'Message Boxes settings';
+$txt['permissiongroup_mboxes_use'] = 'Message Boxes permissions';
+$txt['permissionname_mboxes_use'] = 'Select Message Boxes on the editor';
+$txt['permissionname_mboxes_use_desc'] = 'This will not prevent users from manually typing the BBC';
+$txt['groups_mboxes_use'] = 'Select Message Boxes on the editor';
+$txt['permissionhelp_mboxes_use'] = 'If the user can select the Message Boxes.';
+$txt['permissionhelp_groups_mboxes_use'] = 'If the user can select the Message Boxes.';
+$txt['cannot_mboxes_use'] = 'You\'re not allowed to select the Message Boxes.';
+$txt['mboxes_style'] = 'Boxes icons';
+$txt['mboxes_types']  = [
+	'classic'   => 'Classic',
+	'modern'     => 'Modern',
+	'octagon'       => 'Modern Octagon',
+];
+
+?>
---
+++ Themes/default/languages/mboxes/.spanish_es.php
@@ -0,0 +1,25 @@
+<?php
+
+// BBC
+$txt['error_bbc'] = 'Agregar caja de Error';
+$txt['warning_bbc'] = 'Agregar caja de Advertencia';
+$txt['okay_bbc'] = 'Agregar caja de Éxito';
+$txt['info_bbc'] = 'Agregar caja de Información';
+
+// Settings
+$txt['mboxes_settings'] = 'Ajustes de Message Boxes';
+$txt['permissiongroup_mboxes_use'] = 'Permisos';
+$txt['permissionname_mboxes_use'] = 'Seleccionar cajas de mensaje en el editor';
+$txt['permissionname_mboxes_use_desc'] = 'Esto no previene que el usuario escriba manualmente el BBC';
+$txt['groups_mboxes_use'] = 'Seleccionar cajas de mensaje en el editor';
+$txt['permissionhelp_mboxes_use'] = 'Si el usuario puede seleccionar cajas de mensaje en el editor.';
+$txt['permissionhelp_groups_mboxes_use'] = 'Si el usuario puede seleccionar cajas de mensaje en el editor.';
+$txt['cannot_mboxes_use'] = 'No tienes permiso de seleccionar cajas';
+$txt['mboxes_style'] = 'Iconos de las cajas';
+$txt['mboxes_types']  = [
+	'classic'   => 'Clásico',
+	'modern'     => 'Moderno',
+	'octagon'       => 'Octágono moderno',
+];
+
+?>
