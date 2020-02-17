6ddbc8888d0524327584843e7046b371a06458f5
Diego Andrés <diegoandres_cortes@outlook.com>
2020-02-17 11:46:29 -0600

    Some languages
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/mboxes/MessageBoxes.spanish_latin.php
@@ -0,0 +1,8 @@
+<?php
+
+$txt['error_bbc'] = 'Agregar mensaje de error';
+$txt['warning_bbc'] = 'Agregar mensaje de advertencia';
+$txt['okay_bbc'] = 'Agregar mensaje de éxito';
+$txt['info_bbc'] = 'Agregar mensaje de información';
+
+?>
