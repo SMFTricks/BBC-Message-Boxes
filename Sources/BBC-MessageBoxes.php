e05e2aa2ab344d564b0b87cf61be1a2ceb806ba4
Diego Andrés <diegoandres_cortes@outlook.com>
2020-02-17 11:46:21 -0600

    WIP, bbc function
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Sources/BBC-MessageBoxes.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @package BBC Message Boxes
+ * @version 2.1
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class MessageBoxes
+{
+	public static function bbc_buttons(&$bbc_tags, &$editor_tag_map)
+	{
+		global $txt;
+
+		// Permission to use these??
+		/** TODO */
+
+		// Message Boxes bits
+		loadLanguage('mboxes/MessageBoxes');
+		loadCSSFile('mboxes.css', array('minimize' => true, 'default_theme' => true));
+		loadJavaScriptFile('mboxes.js', array('minimize' => true, 'default_theme' => true));
+
+		$bbc_tags[count($bbc_tags)-1][] = array();
+		$bbc_tags[count($bbc_tags)-1][] = array(
+			'image' => 'error',
+			'code' => 'error',
+			'description' => $txt['error_bbc']
+		);
+		$bbc_tags[count($bbc_tags)-1][] = array(
+			'image' => 'warning',
+			'code' => 'warning',
+			'description' => $txt['warning_bbc']
+		);
+		$bbc_tags[count($bbc_tags)-1][] = array(
+			'image' => 'okay',
+			'code' => 'okay',
+			'description' => $txt['okay_bbc']
+		);
+		$bbc_tags[count($bbc_tags)-1][] = array(
+			'image' => 'info',
+			'code' => 'info',
+			'description' => $txt['info_bbc']
+		);
+	}
+
+	public static function settings()
+	{
+		
+	}
+}
