d75eb7e9f7204674c41220cfc45a63028ec0a2e1
Diego Andrés <diegoandres_cortes@outlook.com>
2020-02-17 19:39:21 -0600

    update version
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/scripts/mboxes.js
+++ Themes/default/scripts/mboxes.js
@@ -1,3 +1,10 @@
+/**
+ * @package BBC Message Boxes
+ * @version 3.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
 // Defines the command info that SCEditor uses when the user clicks a BBCode's button.
 // See https://www.sceditor.com/documentation/custom-commands for info.
 sceditor.command.set(
